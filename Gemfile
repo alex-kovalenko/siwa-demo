0e2cd435104b4d050ae216d14d78c208a0f03c8f
Aleksandr Kovalenko <aleksandr.kovalenko@nike.com>
2019-10-29 21:29:28 -0700

    Adds auth endpoint

--- Gemfile
+++ Gemfile
@@ -1,4 +1,8 @@
 source 'https://rubygems.org'
+
 ruby '2.4.1'
+
 gem 'sinatra'
 gem 'thin'
+
+gem 'jwt'
--- web.rb
+++ web.rb
@@ -1,5 +1,34 @@
 require 'sinatra'
+require 'jwt'
+
 
 get '/' do
   "Hello, world"
+end
+
+get '/jwt/:token' do
+  key_file = "key/siwa-demo.p8"
+  team_id = "2FYPP5PESL"
+  client_id = "com.sampleapp.master.siwa"
+  key_id = "UW4AYX6DUH"
+  validity_period = 100   # In days. Max 180 (6 months) according to Apple docs.
+
+  private_key = OpenSSL::PKey::EC.new IO.read key_file
+
+  token = JWT.encode(
+  	{
+  		iss: team_id,
+  		iat: Time.now.to_i,
+  		exp: Time.now.to_i + 86400 * validity_period,
+  		aud: "https://appleid.apple.com",
+  		sub: client_id
+  	},
+  	private_key,
+  	"ES256",
+  	{
+  		kid: key_id 
+  	}
+  )
+  
+  "#{token}"
 end
